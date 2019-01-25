17debba91c02d8ff6c44da14f56c592779daeaf0
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2019-01-25 15:25:06 +0100

    Hotfix: The connector now won't stop if the shop url contains "install"

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $loader->add('', CONNECTOR_DIR . '/plugins');
 use \jtl\Connector\Application\Application;
 use \jtl\Connector\Gambio\Gambio;
 
-if (!strpos($_SERVER['REQUEST_URI'], 'install')) {
+if (!strpos($_SERVER['REQUEST_URI'], 'jtlconnector/install')) {
     $connector = Gambio::getInstance();
     $application = Application::getInstance();
     $application->register($connector);
