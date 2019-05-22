f8afba63cfa214c798751b3ae62190c0b836a8b7
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2019-05-22 09:41:39 +0200

    CO-387 #resolved

---
+++ install/loggingConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+$projectdir = $_SERVER['DOCUMENT_ROOT'];
+$logFolder = $projectdir . '/jtlconnector/logs/';
+$downloadFolder = $projectdir . '/jtlconnector/install/';
+
+if (isset($_REQUEST['download'])) {
+    if (file_exists($downloadFolder . 'logs.zip')) {
+        unlink($downloadFolder . 'logs.zip');
+    }
+    
+    $zip = new \ZipArchive();
+    $zip->open($downloadFolder . 'logs.zip', \ZipArchive::CREATE);
+    
+    foreach (scandir($logFolder) as $file) {
+        if ($file !== '.' && $file !== '..' && $file !== '.htaccess') {
+            $zip->addFile($logFolder . $file, $file);
+        }
+    }
+    $zip->close();
+    
+    header('Content-type: application/zip');
+    header('Content-Disposition: attachment; filename="logs.zip"');
+    readfile($downloadFolder . 'logs.zip');
+    
+} elseif (isset($_REQUEST['clear'])) {
+    foreach (scandir($logFolder) as $file) {
+        if ($file !== '.' && $file !== '..' && $file !== '.htaccess') {
+            unlink($logFolder . $file);
+        }
+    }
+    
+    header('Location: /jtlconnector/install/#dev_logging');
+}
--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace jtl\Connector\Gambio\Installer;
 
 use jtl\Connector\Core\Database\Mysql;
@@ -6,75 +7,77 @@ use jtl\Connector\Gambio\Installer\Config;
 
 class Installer
 {
-    private $modules = array(
-        'check' => 'Check',
-        'connector' => 'Connector',
-        'status' => 'Status',
-        'thumbs' => 'ThumbMode',
-        'tax_rate' => 'TaxRate',
-    );
-
+    private $modules = [
+        'check'       => 'Check',
+        'connector'   => 'Connector',
+        'status'      => 'Status',
+        'thumbs'      => 'ThumbMode',
+        'tax_rate'    => 'TaxRate',
+        'dev_logging' => 'DevLogging',
+    
+    ];
+    
     private $connectorConfig = null;
-
+    
     public function __construct()
     {
         error_reporting(E_ALL ^ E_NOTICE);
         ini_set('display_errors', 1);
-
+        
         $shopConfig = $this->readConfigFile();
-        $this->connectorConfig = new Config(CONNECTOR_DIR.'/config/config.json');
-
+        $this->connectorConfig = new Config(CONNECTOR_DIR . '/config/config.json');
+        
         $db = Mysql::getInstance();
-
+        
         if (!$db->isConnected()) {
-            $db->connect(array(
-                "host" => $shopConfig['db']["host"],
-                "user" => $shopConfig['db']["user"],
+            $db->connect([
+                "host"     => $shopConfig['db']["host"],
+                "user"     => $shopConfig['db']["user"],
                 "password" => $shopConfig['db']["pass"],
-                "name" => $shopConfig['db']["name"],
-            ));
+                "name"     => $shopConfig['db']["name"],
+            ]);
         }
-
+        
         $db->setNames();
-
-        $moduleInstances = array();
-
+        
+        $moduleInstances = [];
+        
         foreach ($this->modules as $id => $module) {
-            $className = '\\jtl\\Connector\\Gambio\\Installer\\Modules\\'.$module;
+            $className = '\\jtl\\Connector\\Gambio\\Installer\\Modules\\' . $module;
             $moduleInstances[$id] = new $className($db, $this->connectorConfig, $shopConfig);
         }
-
+        
         if ($moduleInstances['check']->hasPassed()) {
             echo '<ul class="nav nav-tabs">';
-
+            
             foreach ($moduleInstances as $class => $instance) {
                 $active = $class == 'check' ? 'active' : '';
-                echo '<li class="'.$active.'"><a href="#'.$class.'" data-toggle="tab"><b>'.$instance::$name.'</b></a></li>';
+                echo '<li class="' . $active . '"><a href="#' . $class . '" data-toggle="tab"><b>' . $instance::$name . '</b></a></li>';
             }
-
+            
             echo '</ul>
 	        	<br>
 	        	<div class="tab-content">';
-
-            $moduleErrors = array();
-
+            
+            $moduleErrors = [];
+            
             foreach ($moduleInstances as $class => $instance) {
                 $active = $class == 'check' ? ' active' : '';
-
+                
                 if (isset($_REQUEST['save'])) {
                     $moduleSave = $instance->save();
                     if ($moduleSave !== true) {
                         $moduleErrors[] = $moduleSave;
                     }
                 }
-
-                echo '<div class="tab-pane'.$active.'" id="'.$class.'">';
+                
+                echo '<div class="tab-pane' . $active . '" id="' . $class . '">';
                 echo $instance->form();
                 echo '</div>';
             }
-
+            
             echo '</div>';
-
+            
             if (isset($_REQUEST['save'])) {
                 if (count($moduleErrors) == 0) {
                     if (!$this->connectorConfig->save()) {
@@ -89,42 +92,42 @@ class Installer
                     echo '<div class="alert alert-danger">Folgende Fehler traten auf:
 		        		<br>
 		        		<ul>';
-
+                    
                     foreach ($moduleErrors as $error) {
-                        echo '<li>'.$error.'</li>';
+                        echo '<li>' . $error . '</li>';
                     }
-
+                    
                     echo '</ul>
 		        		</div>';
                 }
             }
-
+            
             echo '<button type="submit" name="save" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-save"></span> Connector Konfiguration speichern</button>';
         } else {
             echo '<div class="alert alert-danger">Bitte beheben Sie die angezeigten Fehler bevor Sie mit der Konfiguration fortfahren können.</div>';
             echo $moduleInstances['check']->form();
         }
     }
-
+    
     private function readConfigFile()
     {
         require_once realpath(CONNECTOR_DIR . '/../') . '/includes/configure.php';
-
-        return array(
-            'shop' => array(
-                'url' => HTTP_SERVER,
-                'folder' => DIR_WS_CATALOG,
-                'fullUrl' => HTTP_SERVER.DIR_WS_CATALOG,
-            ),
-            'db' => array(
+        
+        return [
+            'shop' => [
+                'url'     => HTTP_SERVER,
+                'folder'  => DIR_WS_CATALOG,
+                'fullUrl' => HTTP_SERVER . DIR_WS_CATALOG,
+            ],
+            'db'   => [
                 'host' => DB_SERVER,
                 'name' => DB_DATABASE,
                 'user' => DB_SERVER_USERNAME,
                 'pass' => DB_SERVER_PASSWORD,
-            ),
-            'img' => array(
+            ],
+            'img'  => [
                 'original' => DIR_WS_ORIGINAL_IMAGES,
-            )
-        );
+            ],
+        ];
     }
 }
