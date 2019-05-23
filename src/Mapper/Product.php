7aeefe7cc5e2ae4a730c51f8749743df6a68cf1a
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2019-05-23 11:58:56 +0200

    Fixed codereview comments

--- src/Mapper/Product.php
+++ src/Mapper/Product.php
@@ -481,7 +481,7 @@ class Product extends BaseMapper
             $quantityProduct->quantity_unit_id = $id;
 
             $this->db->deleteInsertRow($quantityProduct, 'products_quantity_unit', 'products_id', $quantityProduct->products_id);
-        } elseif($id === null) {
+        } else{
             $this->db->query('DELETE FROM products_quantity_unit WHERE products_id=' . $data->getId()->getEndpoint());
         }
     }
