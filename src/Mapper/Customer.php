dbc55c6f3318801b0bf0a956f06bf82caf461f28
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2020-01-13 08:46:14 +0100

    The customers field "hasCustomerAccount" will now be set in relation to the customers "account_type"

--- src/Mapper/Customer.php
+++ src/Mapper/Customer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace jtl\Connector\Gambio\Mapper;
 
 use jtl\Connector\Gambio\Mapper\BaseMapper;
@@ -7,60 +8,71 @@ use jtl\Connector\Core\Utilities\Country;
 
 class Customer extends BaseMapper
 {
-    protected $mapperConfig = array(
-        "table" => "customers",
-        "query" => "SELECT * FROM customers c
+    protected $mapperConfig = [
+        "table"    => "customers",
+        "query"    => "SELECT * FROM customers c
             LEFT JOIN address_book a ON c.customers_default_address_id = a.address_book_id
             LEFT JOIN countries co ON co.countries_id = a.entry_country_id
             LEFT JOIN coupon_gv_customer g ON g.customer_id = c.customers_id
             LEFT JOIN jtl_connector_link_customer l ON c.customers_id = l.endpoint_id
             WHERE l.host_id IS NULL && c.customers_status != 0
             ORDER BY c.customers_date_added",
-        "where" => "customers_id",
+        "where"    => "customers_id",
         "identity" => "getId",
-        "mapPull" => array(
-            "id" => "customers_id",
-            "customerGroupId" => "customers_status",
-            "customerNumber" => "customers_cid",
-            "salutation" => null,
-            "birthday" => null,
-            "firstName" => "customers_firstname",
-            "lastName" => "customers_lastname",
-            "company" => "entry_company",
-            "street" => null,
-            "extraAddressLine" => "entry_additional_info",
-            "zipCode" => "entry_postcode",
-            "city" => "entry_city",
-            "countryIso" => null,
-            "languageISO" => null,
-            "phone" => "customers_telephone",
-            "fax" => "customers_fax",
-            "eMail" => "customers_email_address",
-            "vatNumber" => "customers_vat_id",
+        "mapPull"  => [
+            "id"                        => "customers_id",
+            "customerGroupId"           => "customers_status",
+            "customerNumber"            => "customers_cid",
+            "salutation"                => null,
+            "birthday"                  => null,
+            "firstName"                 => "customers_firstname",
+            "lastName"                  => "customers_lastname",
+            "company"                   => "entry_company",
+            "street"                    => null,
+            "extraAddressLine"          => "entry_additional_info",
+            "zipCode"                   => "entry_postcode",
+            "city"                      => "entry_city",
+            "countryIso"                => null,
+            "languageISO"               => null,
+            "phone"                     => "customers_telephone",
+            "fax"                       => "customers_fax",
+            "eMail"                     => "customers_email_address",
+            "vatNumber"                 => "customers_vat_id",
             "hasNewsletterSubscription" => null,
-            "creationDate" => "customers_date_added",
-            "accountCredit" => "amount"
-        ),
-        "mapPush" => array(
-            "customers_id" => "id",
-            "customers_status" => "customerGroupId",
-            "customers_cid" => "customerNumber",
-            "customers_gender" => null,
-            "customers_firstname" => "firstName",
-            "customers_lastname" => "lastName",
-            "customers_dob" => "birthday",
-            "customers_telephone" => "phone",
-            "customers_fax" => "fax",
+            "creationDate"              => "customers_date_added",
+            "accountCredit"             => "amount",
+            "hasCustomerAccount"        => null
+        ],
+        "mapPush"  => [
+            "customers_id"            => "id",
+            "customers_status"        => "customerGroupId",
+            "customers_cid"           => "customerNumber",
+            "customers_gender"        => null,
+            "customers_firstname"     => "firstName",
+            "customers_lastname"      => "lastName",
+            "customers_dob"           => "birthday",
+            "customers_telephone"     => "phone",
+            "customers_fax"           => "fax",
             "customers_email_address" => "eMail",
-            "customers_vat_id" => "vatNumber",
-            "customers_newsletter" => "hasNewsletterSubscription",
-            "customers_date_added" => "creationDate",
-            "customers_password" => null
-        )
-    );
+            "customers_vat_id"        => "vatNumber",
+            "customers_newsletter"    => "hasNewsletterSubscription",
+            "customers_date_added"    => "creationDate",
+            "customers_password"      => null
+        ],
+    ];
     
-    protected function street($data){
-        if ($this->shopConfig['settings']['ACCOUNT_SPLIT_STREET_INFORMATION'] === 1 && !empty($data["entry_house_number"])){
+    /**
+     * @param $data
+     * @return bool
+     */
+    protected function hasCustomerAccount($data)
+    {
+        return (bool) !$data["account_type"];
+    }
+    
+    protected function street($data)
+    {
+        if ($this->shopConfig['settings']['ACCOUNT_SPLIT_STREET_INFORMATION'] === 1 && !empty($data["entry_house_number"])) {
             return sprintf("%s %s", $data["entry_street_address"], $data["entry_house_number"]);
         } else {
             return $data["entry_street_address"];
@@ -87,7 +99,7 @@ class Customer extends BaseMapper
             return 'w';
         }
     }
-
+    
     protected function customers_gender($data)
     {
         if ($data->getSalutation() == 'm') {
@@ -96,54 +108,54 @@ class Customer extends BaseMapper
             return 'f';
         }
     }
-
+    
     protected function languageISO($data)
     {
         if (!empty($data['countries_iso_code_2'])) {
             return $this->fullLocale(strtolower($data['countries_iso_code_2']));
         }
     }
-
+    
     protected function countryIso($data)
     {
         return Country::map(strtolower($data['countries_iso_code_2']));
     }
-
+    
     protected function hasNewsletterSubscription($data)
     {
         return (is_null($data['customers_newsletter']) || $data['customers_newsletter'] == 0) ? false : true;
     }
-
+    
     protected function customers_password($data)
     {
         $id = $data->getId()->getEndpoint();
-
+        
         if (!empty($id)) {
-            $password = $this->db->query('SELECT customers_password FROM customers WHERE customers_id = '.$id);
+            $password = $this->db->query('SELECT customers_password FROM customers WHERE customers_id = ' . $id);
             $password = $password[0]['customers_password'];
         }
-
+        
         return isset($password) ? $password : md5(rand());
     }
-
+    
     public function push($data, $dbObj = null)
     {
         $id = $data->getId()->getEndpoint();
         
         if (!is_null($id)) {
-            $this->db->query('DELETE FROM address_book WHERE customers_id='.$id);
-            $this->db->query('DELETE FROM customers_info WHERE customers_info_id='.$id);
-            $this->db->query('DELETE FROM coupon_gv_customer WHERE customer_id='.$id);
+            $this->db->query('DELETE FROM address_book WHERE customers_id=' . $id);
+            $this->db->query('DELETE FROM customers_info WHERE customers_info_id=' . $id);
+            $this->db->query('DELETE FROM coupon_gv_customer WHERE customer_id=' . $id);
         }
-
+        
         $return = parent::push($data, $dbObj);
-
+        
         $dataIso = $data->getCountryIso();
-
+        
         if (!empty($dataIso)) {
-            $countryResult = $this->db->query('SELECT countries_id FROM countries WHERE countries_iso_code_2="'.$dataIso.'"');
+            $countryResult = $this->db->query('SELECT countries_id FROM countries WHERE countries_iso_code_2="' . $dataIso . '"');
         }
-
+        
         $entry = new \stdClass();
         $entry->customers_id = $data->getId()->getEndpoint();
         $entry->entry_gender = $data->getSalutation() == 'Herr' ? 'm' : 'f';
@@ -152,9 +164,9 @@ class Customer extends BaseMapper
         $entry->entry_lastname = $data->getLastName();
         
         $entry->entry_street_address = $data->getStreet();
-        if($this->shopConfig['settings']['ACCOUNT_SPLIT_STREET_INFORMATION'] === 1){
+        if ($this->shopConfig['settings']['ACCOUNT_SPLIT_STREET_INFORMATION'] === 1) {
             preg_match('/([[:alnum:]]+)(\\.|\\:|\\-|\\s)*([[:digit:]]+.*)/', $data->getStreet(), $addressData);
-            if (isset($addressData[1]) && isset($addressData[3])){
+            if (isset($addressData[1]) && isset($addressData[3])) {
                 $entry->entry_street_address = $addressData[1];
                 $entry->entry_house_number = $addressData[3];
             }
@@ -165,61 +177,60 @@ class Customer extends BaseMapper
         $entry->entry_city = $data->getCity();
         $entry->entry_state = $data->getState();
         $entry->entry_country_id = ($countryResult) ? $countryResult[0]['countries_id'] : '81';
-
+        
         $address = $this->db->insertRow($entry, 'address_book');
-
+        
         $customerUpdate = new \stdClass();
         $customerUpdate->customers_default_address_id = $address->getKey();
-
+        
         $insertResult = $this->db->updateRow($customerUpdate, $this->mapperConfig['table'], 'customers_id', $data->getId()->getEndpoint());
-
+        
         $addressUpdate = new \stdClass();
         $addressUpdate->customers_id = $data->getId()->getEndpoint();
-
+        
         $this->db->updateRow($addressUpdate, 'address_book', 'address_book_id', $address->getKey());
-
+        
         $infoObj = new \stdClass();
         $infoObj->customers_info_id = $insertResult->getKey();
         $this->db->insertRow($infoObj, 'customers_info');
-
+        
         if ($data->getAccountCredit() > 0) {
             $credit = new \stdClass();
             $credit->customer_id = $insertResult->getKey();
             $credit->amount = $data->getAccountCredit();
             $this->db->insertRow($credit, 'coupon_gv_customer');
         }
-
+        
         return $return;
     }
-
+    
     public function delete($data)
     {
         try {
-            $this->db->query('DELETE FROM customers WHERE customers_id='.$data->getId()->getEndpoint());
-            $this->db->query('DELETE FROM address_book WHERE customers_id='.$data->getId()->getEndpoint());
-            $this->db->query('DELETE FROM customers_info WHERE customers_info_id='.$data->getId()->getEndpoint());
-            $this->db->query('DELETE FROM coupon_gv_customer WHERE customer_id='.$data->getId()->getEndpoint());
-    		
-    		//$this->db->query('DELETE FROM jtl_connector_link WHERE type=2 && endpointId='.$data->getId()->getEndpoint());
+            $this->db->query('DELETE FROM customers WHERE customers_id=' . $data->getId()->getEndpoint());
+            $this->db->query('DELETE FROM address_book WHERE customers_id=' . $data->getId()->getEndpoint());
+            $this->db->query('DELETE FROM customers_info WHERE customers_info_id=' . $data->getId()->getEndpoint());
+            $this->db->query('DELETE FROM coupon_gv_customer WHERE customer_id=' . $data->getId()->getEndpoint());
+            
+            //$this->db->query('DELETE FROM jtl_connector_link WHERE type=2 && endpointId='.$data->getId()->getEndpoint());
+        } catch (\Exception $e) {
         }
-        catch(\Exception $e) {            
-        }
-
+        
         return $data;
     }
-
+    
     public function statistic()
     {
         $count = 0;
-    
+        
         $result = $this->db->query('SELECT COUNT(*) AS count FROM customers c
             LEFT JOIN jtl_connector_link_customer l ON c.customers_id = l.endpoint_id
             WHERE l.host_id IS NULL && c.customers_status != 0');
-    
+        
         if ($result && count($result) > 0) {
             $count = (int)$result[0]['count'];
         }
-    
+        
         return $count;
     }
 }
