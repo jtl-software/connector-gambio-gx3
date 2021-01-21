e7ea60ce479f4bb15e48bec5243f5d18158c8000
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2021-01-21 12:39:16 +0100

    Revised payment mapper

--- src/Mapper/Payment.php
+++ src/Mapper/Payment.php
@@ -8,76 +8,76 @@ use jtl\Connector\Payment\PaymentTypes;
 class Payment extends \jtl\Connector\Gambio\Mapper\BaseMapper
 {
     protected $mapperConfig = [
-        "table"    => "jtl_connector_payment",
-        "query"    => "SELECT p.* FROM jtl_connector_payment p
+        "table" => "jtl_connector_payment",
+        "query" => "SELECT p.* FROM jtl_connector_payment p
             LEFT JOIN jtl_connector_link_payment l ON p.id = l.endpoint_id
             WHERE l.host_id IS NULL",
-        "where"    => "id",
+        "where" => "id",
         "identity" => "getId",
-        "mapPull"  => [
-            "id"                => "id",
-            "customerOrderId"   => "customerOrderId",
-            "billingInfo"       => "billingInfo",
-            "creationDate"      => "creationDate",
-            "totalSum"          => "totalSum",
-            "transactionId"     => "transactionId",
+        "mapPull" => [
+            "id" => "id",
+            "customerOrderId" => "customerOrderId",
+            "billingInfo" => "billingInfo",
+            "creationDate" => "creationDate",
+            "totalSum" => "totalSum",
+            "transactionId" => "transactionId",
             "paymentModuleCode" => "paymentModuleCode",
         ],
     ];
-    
+
     private static $paymentMapping = [
-        'cash'                      => PaymentTypes::TYPE_CASH,
-        'klarna_SpecCamp'           => PaymentTypes::TYPE_KLARNA,
-        'klarna_invoice'            => PaymentTypes::TYPE_KLARNA,
-        'klarna_partPayment'        => PaymentTypes::TYPE_KLARNA,
-        'moneyorder'                => PaymentTypes::TYPE_PREPAYMENT,
-        'banktransfer'              => PaymentTypes::TYPE_BANK_TRANSFER,
-        'cod'                       => PaymentTypes::TYPE_CASH_ON_DELIVERY,
-        'paypal3'                   => PaymentTypes::TYPE_PAYPAL,
-        'amoneybookers'             => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_giropay'      => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_ideal'        => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_mae'          => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_netpay'       => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_psp'          => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_pwy'          => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_sft'          => PaymentTypes::TYPE_SKRILL,
-        'moneybookers_wlt'          => PaymentTypes::TYPE_SKRILL,
-        'invoice'                   => PaymentTypes::TYPE_INVOICE,
+        'cash' => PaymentTypes::TYPE_CASH,
+        'klarna_SpecCamp' => PaymentTypes::TYPE_KLARNA,
+        'klarna_invoice' => PaymentTypes::TYPE_KLARNA,
+        'klarna_partPayment' => PaymentTypes::TYPE_KLARNA,
+        'moneyorder' => PaymentTypes::TYPE_PREPAYMENT,
+        'banktransfer' => PaymentTypes::TYPE_BANK_TRANSFER,
+        'cod' => PaymentTypes::TYPE_CASH_ON_DELIVERY,
+        'paypal3' => PaymentTypes::TYPE_PAYPAL,
+        'amoneybookers' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_giropay' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_ideal' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_mae' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_netpay' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_psp' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_pwy' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_sft' => PaymentTypes::TYPE_SKRILL,
+        'moneybookers_wlt' => PaymentTypes::TYPE_SKRILL,
+        'invoice' => PaymentTypes::TYPE_INVOICE,
         'sofort_sofortueberweisung' => PaymentTypes::TYPE_SOFORT,
-        'worldpay'                  => PaymentTypes::TYPE_WORLDPAY,
+        'worldpay' => PaymentTypes::TYPE_WORLDPAY,
         //HUB TYPES
-        'CashHub'                   => PaymentTypes::TYPE_CASH,
-        'CashOnDeliveryHub'         => PaymentTypes::TYPE_CASH_ON_DELIVERY,
-        'InvoiceHub'                => PaymentTypes::TYPE_INVOICE,
-        'KlarnaPaylaterHub'         => PaymentTypes::TYPE_KLARNA,
-        'KlarnaPaynowHub'           => PaymentTypes::TYPE_KLARNA,
-        'KlarnaSliceitHub'          => PaymentTypes::TYPE_KLARNA,
-        'KlarnaBanktrankferHub'     => PaymentTypes::TYPE_KLARNA,
-        'MoneyOrderHub'             => PaymentTypes::TYPE_PREPAYMENT,
-        'MoneyOrderPlusHub'         => PaymentTypes::TYPE_PREPAYMENT,
-        'PayPalHub'                 => PaymentTypes::TYPE_PAYPAL_PLUS,
-        'PayPal2Hub'                 => PaymentTypes::TYPE_PAYPAL_PLUS,
-        'SofortHub'                 => PaymentTypes::TYPE_SOFORT,
-        'WirecardCreditcardHub'     => PaymentTypes::TYPE_WIRECARD,
-        'WirecardInvoiceHub'        => PaymentTypes::TYPE_WIRECARD,
-        'WirecardSepaddHub'         => PaymentTypes::TYPE_WIRECARD,
-        'WirecardSofortbankingHub'  => PaymentTypes::TYPE_WIRECARD,
-        'WirecardWiretransferHub'   => PaymentTypes::TYPE_WIRECARD,
+        'CashHub' => PaymentTypes::TYPE_CASH,
+        'CashOnDeliveryHub' => PaymentTypes::TYPE_CASH_ON_DELIVERY,
+        'InvoiceHub' => PaymentTypes::TYPE_INVOICE,
+        'KlarnaPaylaterHub' => PaymentTypes::TYPE_KLARNA,
+        'KlarnaPaynowHub' => PaymentTypes::TYPE_KLARNA,
+        'KlarnaSliceitHub' => PaymentTypes::TYPE_KLARNA,
+        'KlarnaBanktrankferHub' => PaymentTypes::TYPE_KLARNA,
+        'MoneyOrderHub' => PaymentTypes::TYPE_PREPAYMENT,
+        'MoneyOrderPlusHub' => PaymentTypes::TYPE_PREPAYMENT,
+        'PayPalHub' => PaymentTypes::TYPE_PAYPAL_PLUS,
+        'PayPal2Hub' => PaymentTypes::TYPE_PAYPAL_PLUS,
+        'SofortHub' => PaymentTypes::TYPE_SOFORT,
+        'WirecardCreditcardHub' => PaymentTypes::TYPE_WIRECARD,
+        'WirecardInvoiceHub' => PaymentTypes::TYPE_WIRECARD,
+        'WirecardSepaddHub' => PaymentTypes::TYPE_WIRECARD,
+        'WirecardSofortbankingHub' => PaymentTypes::TYPE_WIRECARD,
+        'WirecardWiretransferHub' => PaymentTypes::TYPE_WIRECARD,
     ];
-    
+
     /**
      * Payment constructor.
      */
     public function __construct()
     {
         parent::__construct();
-        
+
         if (!empty($this->connectorConfig->from_date)) {
             $this->mapperConfig['query'] .= ' && creationDate >= "' . $this->connectorConfig->from_date . '"';
         }
     }
-    
+
     /**
      * @param null $parent
      * @param null $limit
@@ -87,21 +87,21 @@ class Payment extends \jtl\Connector\Gambio\Mapper\BaseMapper
     public function pull($parent = null, $limit = null)
     {
         $additional = [];
-        
+
         $payments = $this->db->query('SELECT p.* FROM jtl_connector_payment p
             LEFT JOIN jtl_connector_link_payment l ON p.id = l.endpoint_id
             WHERE l.host_id IS NULL');
-        
+
         foreach ($payments as $payment) {
             $additional[] = $this->generateModel($payment);
         }
-        
+
         $result = array_merge(
             $this->paypal(),
             $this->hubPayments(),
             $additional
         );
-        
+
         return $result;
     }
 
@@ -121,30 +121,28 @@ class Payment extends \jtl\Connector\Gambio\Mapper\BaseMapper
     private function paypal(): array
     {
         $return = [];
-        
-        $sql = "
-            SELECT p.orders_id order_id, p.payment_id transaction_id, o.date_purchased creation_date, o.payment_method, t.value total_sum
-            FROM orders_paypal_payments p
-            LEFT JOIN jtl_connector_link_payment l ON p.payment_id = l.endpoint_id
-            LEFT JOIN orders o ON o.orders_id = p.orders_id
-            LEFT JOIN orders_total t ON t.orders_id = p.orders_id AND t.class = 'ot_total'
-        ";
-        
+
+        $sql = 'SELECT p.orders_id as order_id, p.payment_id as transaction_id, o.date_purchased as payment_date, o.payment_method as payment_method, t.value as total_sum
+                FROM orders_paypal_payments p
+                LEFT JOIN jtl_connector_link_payment l ON p.payment_id = l.endpoint_id
+                LEFT JOIN orders o ON o.orders_id = p.orders_id
+                LEFT JOIN orders_total t ON t.orders_id = p.orders_id AND t.class = \'ot_total\'';
+
         $results = $this->db->query($sql);
-        
+
         foreach ($results as $paymentData) {
             $return[] = (new PaymentModel())
-                ->setCreationDate(new \DateTime($paymentData['creation_date']))
+                ->setCreationDate(new \DateTime($paymentData['payment_date']))
                 ->setCustomerOrderId($this->identity($paymentData['order_id']))
                 ->setId($this->identity($paymentData['transaction_id']))
-                ->setPaymentModuleCode(self::mapPaymentType($paymentData['payment_method']))
+                ->setPaymentModuleCode(self::mapPaymentType($paymentData['payment_method'] ?? 'unknown'))
                 ->setTotalSum(floatval($paymentData['total_sum']))
                 ->setTransactionId($paymentData['transaction_id']);
         }
-        
+
         return $return;
     }
-    
+
     /**
      * @return array
      * @throws \Exception
@@ -152,24 +150,25 @@ class Payment extends \jtl\Connector\Gambio\Mapper\BaseMapper
     private function hubPayments(): array
     {
         $return = [];
-        
-        $results = $this->db->query('SELECT *
-            FROM orders o
-              LEFT JOIN orders_total ot ON o.orders_id = ot.orders_id
-              LEFT JOIN jtl_connector_link_payment l ON o.gambio_hub_transaction_code = l.endpoint_id
-            WHERE l.host_id IS NULL AND o.payment_method = "gambio_hub" AND ot.class = "ot_total"
-            ');
-        
+
+        $sql = 'SELECT *
+                FROM orders o
+                LEFT JOIN orders_total ot ON o.orders_id = ot.orders_id
+                LEFT JOIN jtl_connector_link_payment l ON o.gambio_hub_transaction_code = l.endpoint_id
+                WHERE l.host_id IS NULL AND o.payment_method = "gambio_hub" AND ot.class = \'ot_total\'';
+
+        $results = $this->db->query($sql);
+
         foreach ($results as $paymentData) {
             $return[] = (new PaymentModel())
                 ->setCreationDate(new \DateTime($paymentData['date_purchased']))
                 ->setCustomerOrderId($this->identity($paymentData['orders_id']))
                 ->setId($this->identity($paymentData['gambio_hub_transaction_code']))
-                ->setPaymentModuleCode(self::mapPaymentType($paymentData["gambio_hub_module"]))
+                ->setPaymentModuleCode(self::mapPaymentType($paymentData['gambio_hub_module'] ?? 'unknown'))
                 ->setTotalSum(floatval($paymentData['value']))
                 ->setTransactionId($paymentData['gambio_hub_transaction_code']);
         }
-        
+
         return $return;
     }
 
@@ -183,7 +182,7 @@ class Payment extends \jtl\Connector\Gambio\Mapper\BaseMapper
         if ($toJtl === false) {
             return array_flip(self::$paymentMapping)[$moduleCode] ?? $moduleCode;
         }
-        
+
         return self::$paymentMapping[$moduleCode] ?? $moduleCode;
     }
 }
