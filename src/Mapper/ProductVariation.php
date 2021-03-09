80310913bb5750919bf60151e042525c3f7746af
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2021-03-09 10:09:15 +0100

    formatted and extracted sql queries to variables

--- src/Mapper/ProductVariation.php
+++ src/Mapper/ProductVariation.php
@@ -280,38 +280,42 @@ class ProductVariation extends Product
                     if (!empty($productId) && !empty($newVariationValues)) {
                         try {
                             $this->db->DB()->begin_transaction();
-                            $unusedProductPropertiesValuesSql = sprintf('
-                            SELECT ppcv.* FROM products_properties_combis_values AS ppcv
-                            LEFT JOIN products_properties_combis AS ppc ON ppc.products_properties_combis_id = ppcv.products_properties_combis_id
-                            LEFT JOIN properties_values AS pv ON pv.properties_values_id = ppcv.properties_values_id
-                            WHERE pv.properties_values_id NOT IN (%s) AND ppc.products_id = %s', join(',', $newVariationValues), $productId);
+                            $sql =
+                                'SELECT ppcv.*' . "\n" .
+                                'FROM products_properties_combis_values AS ppcv' . "\n" .
+                                'LEFT JOIN products_properties_combis AS ppc ON ppc.products_properties_combis_id = ppcv.products_properties_combis_id' . "\n" .
+                                'LEFT JOIN properties_values AS pv ON pv.properties_values_id = ppcv.properties_values_id' . "\n" .
+                                'WHERE pv.properties_values_id NOT IN (%s) AND ppc.products_id = %s';
 
-                            $unusedProductPropertiesValues = $this->db->query($unusedProductPropertiesValuesSql);
+                            $unusedProductPropertiesValues = $this->db->query(sprintf($sql, join(',', $newVariationValues), $productId));
+
                             $productPropertiesCombisValuesId = array_column($unusedProductPropertiesValues, 'products_properties_combis_values_id');
-                            $propertiesValuesId = array_column($unusedProductPropertiesValues, 'properties_values_id');
+                            $propertiesValuesId = array_unique(array_column($unusedProductPropertiesValues, 'properties_values_id'));
 
                             if (!empty($productPropertiesCombisValuesId)) {
-
-                                $this->db->query(sprintf('DELETE FROM products_properties_combis_values WHERE products_properties_combis_values_id IN (%s)',
-                                    join(',', $productPropertiesCombisValuesId)));
+                                $sql = 'DELETE FROM products_properties_combis_values WHERE products_properties_combis_values_id IN (%s)';
+                                $this->db->query(sprintf($sql, join(',', $productPropertiesCombisValuesId)));
 
                                 $propertyValue = null;
                                 foreach ($propertiesValuesId as $propertyValueId) {
-                                    $sharedPropertiesValues = $this->db->query(sprintf('SELECT * FROM products_properties_combis_values WHERE properties_values_id = %s', $propertyValueId));
+                                    $sql = 'SELECT * FROM products_properties_combis_values WHERE properties_values_id = %s';
+                                    $sharedPropertiesValues = $this->db->query(sprintf($sql, $propertyValueId));
 
                                     if (empty($sharedPropertiesValues)) {
-                                        $propertyValue = $this->db->query(
-                                            sprintf('SELECT * FROM properties_values WHERE properties_id IN (SELECT properties_id FROM properties_values WHERE properties_values_id = %s)', $propertyValueId)
-                                        );
+                                        $sql = 'SELECT * FROM properties_values WHERE properties_id IN (SELECT properties_id FROM properties_values WHERE properties_values_id = %s)';
+                                        $propertyValue = $this->db->query(sprintf($sql, $propertyValueId));
 
-                                        $this->db->query(
-                                            sprintf('DELETE properties_values, properties_values_description FROM properties_values 
-                                         LEFT JOIN properties_values_description ON properties_values_description.properties_values_id = properties_values.properties_values_id                                
-                                         WHERE properties_values.properties_values_id = %s', $propertyValueId)
-                                        );
+                                        $sql =
+                                            'DELETE properties_values, properties_values_description' . "\n" .
+                                            'FROM properties_values' . "\n" .
+                                            'LEFT JOIN properties_values_description ON properties_values_description.properties_values_id = properties_values.properties_values_id' . "\n" .
+                                            'WHERE properties_values.properties_values_id = %s';
+
+                                        $this->db->query(sprintf($sql, $propertyValueId));
 
                                         if (is_array($propertyValue) && count($propertyValue) === 1) {
-                                            $this->db->query(sprintf('DELETE FROM properties WHERE properties_id IN (%s)', join(',', array_column($propertyValue, 'properties_id'))));
+                                            $sql = 'DELETE FROM properties WHERE properties_id IN (%s)';
+                                            $this->db->query(sprintf($sql, join(',', array_column($propertyValue, 'properties_id'))));
                                         }
                                     }
                                 }
